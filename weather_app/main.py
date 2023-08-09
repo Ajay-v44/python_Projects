39540567387313bebc7dd8f646483aff3d964e50
Ajay v <vajay4834@gmail.com>
2023-08-09 12:52:08 +0530

    added pyttsx3 to weather app

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,6 +1,13 @@
 import requests
-
+import json
+import pyttsx3
 city=input("Enter the name of the city \n")
 url=f"http://api.weatherapi.com/v1/current.json?key=49ca892080274a0296965642230908&q={city}"
 resp=requests.get(url)
 print(resp.text)
+wdic= json.loads(resp.text)
+temp=wdic["current"]["temp_c"]
+engine = pyttsx3.init()
+engine.say(f"the current weather at {city} is {temp} degrees")
+engine.runAndWait()
+
